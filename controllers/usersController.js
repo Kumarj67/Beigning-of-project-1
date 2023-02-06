6b4b24f0583fae53609ddb1d922ae2cc16e40dbc
Jeetesh <jeeteshkumar45@gmail.com>
2023-02-06 23:50:26 +0530

    Seventh Commit-Signin/signip

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,9 +2,21 @@ const User = require("../models/user");
 
 module.exports.profile = function (req, res) {
   // return res.end("<h1>User's Profile</h1>");
-  return res.render("user_profile", {
-    title: "Profile",
-  });
+  if (req.cookies.user_id) {
+    User.findById(req.cookies.user_id, function (err, user) {
+      if (user) {
+        return res.render("user_profile", {
+          title: "Profile",
+          user: user,
+        });
+      }
+      if (err) {
+        console.log("Error in loading profile page", err);
+      }
+    });
+  } else {
+    return res.redirect("/users/sign-in");
+  }
 };
 // Render the Signup pages
 module.exports.signup = function (req, res) {
@@ -25,13 +37,13 @@ module.exports.create = function (req, res) {
   }
   User.findOne({ email: req.body.email }, function (err, user) {
     if (err) {
-      console.log("error in signup", err);
+      console.log("error in finding the user while signingup", err);
       return;
     }
     if (!user) {
       User.create(req.body, function (err, user) {
         if (err) {
-          console.log("error in signup", err);
+          console.log("error in creating users in signingup", err);
           return;
         }
         return res.redirect("/users/sign-in");
@@ -43,4 +55,26 @@ module.exports.create = function (req, res) {
 };
 
 // get the signin data
-module.exports.createSession = function (req, res) {};
+module.exports.createSession = function (req, res) {
+  // steps to authenticate
+  // find the user
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding the user while signing in", err);
+      return;
+    }
+    //handle user found
+    if (user) {
+      // handle mismatching or password mismatched
+      if (user.password != req.body.password) {
+        return res.redirect("back");
+      }
+      //handle session creation
+      res.cookie("user_id", user.id);
+      return res.redirect("/users/profile");
+    } else {
+      //handle user not found
+      return res.redirect("back");
+    }
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,6 @@ router.get("/sign-in", usersController.signin);
 router.get("/sign-up", usersController.signup);
 //router.get("/create", usersController.create); //method should be same as in form of sign up method is defined as post but we are  using router.get
 router.post("/create", usersController.create);
+router.post("/create-session", usersController.createSession);
 
 module.exports = router;
